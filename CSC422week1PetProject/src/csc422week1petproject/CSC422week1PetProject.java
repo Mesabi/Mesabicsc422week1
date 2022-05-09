3cbdf40dbb943b2a71edef607a22372da5d7c92c
Mesabi <57125799+Mesabi@users.noreply.github.com>
2022-05-09 00:13:22 -0500

    Basic Hello World Message to Test Git.

---
+++ CSC422week1PetProject/src/csc422week1petproject/CSC422week1PetProject.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csc422week1petproject;
+
+/**
+ *
+ * @author mesabi
+ */
+public class CSC422week1PetProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+        
+    }
+    
+}
